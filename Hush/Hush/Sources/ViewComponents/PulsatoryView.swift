6a415bea188ea61118d1951c556862ee6ce58e79
Tomasz Kryg <tomasz.kryg@gmail.com>
2020-08-12 20:17:07 +0200

    Add title label to the pulsatory view

--- Hush/Hush/Sources/ViewComponents/PulsatoryView.swift
+++ Hush/Hush/Sources/ViewComponents/PulsatoryView.swift
@@ -11,9 +11,16 @@ import UIKit
 class PulsatoryView: UIView {
     
     private lazy var circle: Circle = {
-        let view = Circle(frame: .zero)
+        let view = Circle(autoLayout: true)
         view.backgroundColor = UIColor.systemGreen
         return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel(autoLayout: true)
+        label.text = "Sample text"
+        label.textColor = UIColor.white
+        return label
     }()
     
     override public init(frame: CGRect) {
@@ -27,24 +34,31 @@ class PulsatoryView: UIView {
     }
     
     private func commonSetup() {
-        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.white
         addSubview(circle)
+        addSubview(titleLabel)
         setupConstraints()
         startPulseAnimation()
     }
     
     private func setupConstraints() {
-        circle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32.0).isActive = true
-        circle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32.0).isActive = true
+        circle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Margin.wide).isActive = true
+        circle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Margin.wide).isActive = true
         circle.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         circle.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        titleLabel.centerYAnchor.constraint(equalTo: circle.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: circle.centerXAnchor).isActive = true
     }
     
     private func startPulseAnimation() {
-        circle.transform = CGAffineTransform(scaleX: 0.4, y: 0.4)
+        let transform = CGAffineTransform(scaleX: 0.4, y: 0.4)
+        circle.transform = transform
+        titleLabel.transform = transform
+        
         UIView.animate(withDuration: 5.0, delay: 0.0, options: [.autoreverse, .repeat], animations: { [weak self] in
             self?.circle.transform = CGAffineTransform.identity
+            self?.titleLabel.transform = CGAffineTransform.identity
         }, completion: { _ in return })
     }
     
