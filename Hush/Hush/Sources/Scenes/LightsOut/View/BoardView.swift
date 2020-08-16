50e74a9dc5e1745660018341fb6dea36e60c9c56
Tomasz Kryg <tomasz.kryg@gmail.com>
2020-08-16 14:34:26 +0200

    Initialize BoardView layout

---
+++ Hush/Hush/Sources/Scenes/LightsOut/View/BoardView.swift
@@ -0,0 +1,41 @@
+//
+//  BoardView.swift
+//  Hush
+//
+//  Created by Tomasz Kryg on 16/08/2020.
+//  Copyright © 2020 Kryg. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+    lazy var layout: UICollectionViewLayout = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        return layout
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .gray
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonSetup()
+    }
+    
+    private func commonSetup() {
+        addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+    }
+}
